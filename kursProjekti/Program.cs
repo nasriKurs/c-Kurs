505845e257e5a0d051617b57b75bedef3a54df98
Naser Ljajic <naserljajic@gmail.com>
2020-06-24 21:42:38 +0200

    Add project files.

---
+++ kursProjekti/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace kursProjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int broj = 1; broj < 100; broj++)
+            {
+                if ((broj % 3 == 0) && (broj % 5 == 0))
+                    Console.WriteLine("fizzbuzz");
+                else if (broj % 3 == 0)
+                    Console.WriteLine("fizz");
+                else if (broj % 5 == 0)
+                    Console.WriteLine("buzz");
+                else
+                    Console.WriteLine(broj);
+            }
+            
+        }
+    }
+}
